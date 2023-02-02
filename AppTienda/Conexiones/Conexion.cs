e586d035c06e36de8f664e3e66ded0e409e10447
Skierhy <Skierhy@gmail.com>
2023-02-02 12:47:37 -0600

    Agregar archivos de proyecto.

---
+++ AppTienda/Conexiones/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+
+namespace AppTienda.Conexiones
+{
+    internal class Conexion
+    {
+        // FirebaseClient es la clase que nos permite conectarnos a la base de datos
+        public static FirebaseClient conexionFireBase = new FirebaseClient("https://apptiendaxamarin-default-rtdb.firebaseio.com/");
+    }
+}
